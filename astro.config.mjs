0c8bb86cbde8e717974b8122dde0f1443a92d35f
Dhravina <kdhravina@gmail.com>
2026-07-18 19:35:34 +0530

    tailwind downloaded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-// @ts-check
+//@ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site:  "https://dkdk09.github.io"
+  base: "/personal-website/",
   vite: {
     plugins: [tailwindcss()],
   },
